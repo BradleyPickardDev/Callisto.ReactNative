11ab3547cd4a4cabf2bffef4c452697e400badea
microcian <a.rahman.uaf@gmail.com>
2022-07-28 20:49:02 +0500

    white label UI interface completed in IOS project

---
+++ ios/WhiteLabelConfig.h
@@ -0,0 +1,19 @@
+//
+//  WhiteLabelConfig.h
+//  RocketChatRN
+//
+//  Created by Abdul Rahman Majeed on 27/07/2022.
+//  Copyright © 2022 Facebook. All rights reserved.
+//
+
+//#ifndef WhiteLabelConfig_h
+//#define WhiteLabelConfig_h
+//
+//
+//#endif /* WhiteLabelConfig_h */
+#import <Foundation/Foundation.h>
+#import <React/RCTBridgeModule.h>
+
+@interface WhiteLabelConfig : NSObject<RCTBridgeModule>
+
+@end
---
+++ ios/WhiteLabelConfig.m
@@ -0,0 +1,39 @@
+//
+//  WhiteLabelConfig.m
+//  RocketChatRN
+//
+//  Created by Abdul Rahman Majeed on 27/07/2022.
+//  Copyright © 2022 Facebook. All rights reserved.
+//
+
+#import "WhiteLabelConfig.h"
+
+@implementation WhiteLabelConfig
+
+RCT_EXPORT_MODULE(WhiteLabelConfig);
+
+RCT_EXPORT_BLOCKING_SYNCHRONOUS_METHOD(getAppName) {
+//  return [[NSBundle mainBundle] objectForInfoDictionaryKey:@"CFBundleDisplayName"];
+  return NSLocalizedString(@"strJovianApp", @"App name");
+
+}
+
+RCT_EXPORT_BLOCKING_SYNCHRONOUS_METHOD(getGreetingText) {
+  return NSLocalizedString(@"strJovianApp", @"App name");
+}
+
+RCT_EXPORT_BLOCKING_SYNCHRONOUS_METHOD(getLogoPath) {
+  return @"Launch Screen Icon";
+}
+
+RCT_EXPORT_BLOCKING_SYNCHRONOUS_METHOD(getServerPlaceHolder) {
+  return NSLocalizedString(@"strServerUrlPlaceholder", @"App name");
+}
+
+
+
+RCT_EXPORT_BLOCKING_SYNCHRONOUS_METHOD(getClientName) {
+  return NSLocalizedString(@"clientName ", @"App name");
+}
+
+@end
