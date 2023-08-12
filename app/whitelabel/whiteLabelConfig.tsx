86f803451d59a3bb35ff0187bcbc8b6b278c5170
Abe <>
2023-08-13 00:20:21 +0500

    android white label completed and run with change of google play service file

--- app/whitelabel/whiteLabelConfig.tsx
+++ app/whitelabel/whiteLabelConfig.tsx
@@ -1,12 +1,12 @@
 import { NativeModules } from 'react-native';
 
 export default class WhiteLabelConfig {
-	public static readonly APP_NAME: string = NativeModules.WhiteLabelConfig.getAppName();
-	public static readonly CLIENT_NAME: string = NativeModules.WhiteLabelConfig.getClientName();
-	public static readonly GREETING_TEXT: string = NativeModules.WhiteLabelConfig.getGreetingText();
-	public static readonly APP_LOGO_PATH: string = NativeModules.WhiteLabelConfig.getLogoPath();
-	public static readonly SERVER_PLACEHOLDER: string = NativeModules.WhiteLabelConfig.getServerPlaceHolder();
-	public static readonly STORE_APPLICATION_ID: string = NativeModules.WhiteLabelConfig.getStoreApplicationID();
-	public static readonly BUNDLE_ID: string = NativeModules.WhiteLabelConfig.getBundleID();
-	public static readonly CONTACT_US_EMAIL: string = NativeModules.WhiteLabelConfig.getContactUsEmail();
+	public static readonly APP_NAME: string = "Jovian Digital App";
+	public static readonly CLIENT_NAME: string = "Callisto";
+	public static readonly GREETING_TEXT: string = "";
+	public static readonly APP_LOGO_PATH: string = "logo";
+	public static readonly SERVER_PLACEHOLDER: string = "Ex. your-company.joviandigital.com";
+	public static readonly STORE_APPLICATION_ID: string = "com.joviandigital.callisto";
+	public static readonly BUNDLE_ID: string = "com.joviandigital.callisto";
+	public static readonly CONTACT_US_EMAIL: string = "";
 }
